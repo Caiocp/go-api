69d97c342200b5681471cd364e49e44248cba3b4
Caio Carvalho <ccarvalho451@gmail.com>
2022-11-14 16:17:57 -0300

    feat: add product handlers with go-chi router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/caiocp/go-api/configs"
+	"github.com/caiocp/go-api/internal/entities"
+	"github.com/caiocp/go-api/internal/infra/database"
+	"github.com/caiocp/go-api/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	cfg, _ := configs.LoadConfig(".")
-	println(cfg.DBHost)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entities.Product{}, &entities.User{})
+
+	productDB := database.NewProduct(db)
+
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products", productHandler.GetProducts)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	http.ListenAndServe(":8080", r)
 }
